0e923009c48baa937c37e5bd9ee258efc69bc221
Tuan Nguyen <tuan.nguyen@texo.design>
2022-09-13 21:08:32 +0700

    Add Menu to react page

---
+++ resources/js/LeftMenu/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Menu from './menu';
+
+export default function LeftMenu() {
+  return (
+    <Container maxWidth={false}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ height: 50, marginTop: 0, paddingTop: 0 }}
+      >
+        <Menu></Menu>
+      </Grid>
+    </Container>
+  );
+}
