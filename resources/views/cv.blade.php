a54cd5600c1695f1819052720eec9a020073ea27
Tuan Nguyen <tuan.nguyen@texo.design>
2022-12-05 15:53:56 +0700

    Change btn style for skill

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -88,16 +88,11 @@
 
     .skill-btn {
       padding: 4px;
-      color: #fff;
-      background-color: #a9cece;
-      background-image: radial-gradient(93% 87% at 87% 89%, rgba(0, 0, 0, 0.23) 0%, transparent 86.18%), radial-gradient(66% 66% at 26% 20%, rgba(255, 255, 255, 0.55) 0%, rgba(255, 255, 255, 0) 69.79%, rgba(255, 255, 255, 0) 100%);
-      box-shadow: inset -3px -3px 9px rgba(255, 255, 255, 0.25), inset 0px 3px 9px rgba(255, 255, 255, 0.3), inset 0px 1px 1px rgba(255, 255, 255, 0.6), inset 0px -8px 36px rgba(0, 0, 0, 0.3), inset 0px 1px 5px rgba(255, 255, 255, 0.6), 2px 19px 31px rgba(0, 0, 0, 0.2);
+      color: rgb(234, 223, 223);
+      background-color: #5d7a7a;
       border-radius: 3px;
       font-weight: bold;
       border: 0;
-      user-select: none;
-      -webkit-user-select: none;
-      touch-action: manipulation;
       cursor: pointer;
     }
 
@@ -480,10 +475,18 @@
 
     <!-- Right section -->
     <div id="right" class="column">
-      <div style="background-color: #283138; height: 170px; padding-top: 10px;">
-        <div style="text-align: center;">
-          <span id="name">Tuan Nguyen 
-          <span style="font-size: 15px;">05/11/1993</span>
+      <div style="background-color: #283138; height: 260px; padding-top: 10px;">
+        <div style="height: 120px;">
+          <table>
+            <tbody>
+              <tr>
+                <td><span id="name">Tuan Nguyen <br>
+                    <span style="font-size: 15px; margin-left: 45px;">05/11/1993</span>
+                  </span></td>
+                <td><img src="{{url('/images/avatar.jpg')}}" id="avatar"></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
         <div class="contact-div">
