a08eb30df8a3a3822d1919b28c57b6779ffd4bf3
Tuan Nguyen <tuan.nguyen@texo.design>
2022-12-09 21:40:40 +0700

    Change regex for website name

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -108,7 +108,7 @@ class StockController extends Controller
         $vietnameseSignedChars = 'àáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ';
         $companyRegex = "/^[a-zA-Z$vietnameseSignedChars]{1,20}(\s+[1-9a-zA-Z$vietnameseSignedChars\(\)\.\/\\\&\-\+\:\,]{1,20})+$/";
         $needUnique = $method === 'store' ? '|unique:stocks' : '';
-        $websiteRegex = "/^(https?:\/\/)?(www.)?[a-zA-z0-9._]{1,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\/.]*)$/";
+        $websiteRegex = "/^(http(s)?:\/\/)?(www.)?[a-zA-z0-9._\-]{1,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\/.]*)$/";
         $vietstockCrawlLinkRegex = "/^(https:\/\/)?finance.vietstock.vn\/[A-Za-z0-9-:\/\\()\&]{1,256}.htm$/";
 
         return $request->validate([
